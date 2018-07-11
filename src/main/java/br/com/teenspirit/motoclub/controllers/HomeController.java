8201e2e9104f54706e071e381e72ab809b1a662e
gneves <gneves@mmcafe.com.br>
2018-07-11 18:15:26 -0300

    ajustes

--- src/main/java/br/com/teenspirit/motoclub/controllers/HomeController.java
+++ src/main/java/br/com/teenspirit/motoclub/controllers/HomeController.java
@@ -2,23 +2,13 @@ package br.com.teenspirit.motoclub.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Controller
 public class HomeController {
 	
-	public InternalResourceViewResolver internalResourceViewResolver() {
-		
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views");
-		resolver.setSuffix(".jsp");
-		return resolver;
-	}
-	
-	@RequestMapping("/")
-	public String index(){
-		
-		System.out.println("Entrando no site");
-		return "home.jsp";
+@RequestMapping("/")
+public String index(){
+	System.out.println("Entrando no site");
+	return "home";
 	}
 }
